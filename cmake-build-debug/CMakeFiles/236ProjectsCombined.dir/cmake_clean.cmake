ab5c9944aa8578f9d0cee8b176eb482daf6f1750
jaclynpalmer <jaclyn.palmer@byu.edu>
2023-10-23 10:06:51 -0600

    Adding files

---
+++ cmake-build-debug/CMakeFiles/236ProjectsCombined.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "236ProjectsCombined"
+  "236ProjectsCombined.pdb"
+  "CMakeFiles/236ProjectsCombined.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/236ProjectsCombined.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/236ProjectsCombined.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 236ProjectsCombined.
+# This may be replaced when dependencies are built.
